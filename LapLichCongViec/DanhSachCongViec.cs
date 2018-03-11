418cd28f7848dfe0039b6c176a3d970ef78cfea4
= <=>
2018-03-11 22:51:44 +0700

    tao class luu tru cong viec

---
+++ LapLichCongViec/DanhSachCongViec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LapLichCongViec
+{
+    class DanhSachCongViec
+    {
+        private List<MotCongViec> listJob;
+        public List<MotCongViec> ListJob
+        {
+            get { return listJob; }
+            set { listJob = value; }
+        }
+    }
+}
---
+++ LapLichCongViec/MotCongViec.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LapLichCongViec
+{
+    [Serializable]
+    class MotCongViec
+    {
+        private DateTime day;
+        public DateTime Day
+        {
+            get { return day; }
+            set { day = value; }
+        }
+
+        private bool status;
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private Point startTime;
+        public Point StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+
+        private Point endTime;
+        public Point EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+    }
+}
